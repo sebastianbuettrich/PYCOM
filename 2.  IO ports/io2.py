3533ff759f88fe6b801de69ee5f9f09848a77cd9
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-10 15:53:23 +0100

    Update io2.py

--- 2.  IO ports/io2.py
+++ 2.  IO ports/io2.py
@@ -1,39 +1,40 @@
+
 from machine import Pin
 import time
-leds = ['p12','p11','p10','p9']
-# First way
-for i in leds:
-    Pin(leds, mode=Pin.OUT)
-#Second way
-for i in range(len(leds)):
-    Pin(leds[i], mode=Pin.OUT)
+# individual configuration
+led1 = Pin('P12', mode=Pin.OUT) #pin 12 out
+led1 = Pin('P12', mode=Pin.OUT) #pin 12 out
+led2 = Pin('P11', mode=Pin.OUT) #pin 11 out
+led3= Pin('P10', mode=Pin.OUT)  #pin 10 out
+led4 = Pin('P9', mode=Pin.OUT)  #pin 9 out
+toggle=0
 x=0
-y=0
 while True:
-    for j in leds:
-        print("Game 1")
-        j.value(1)
-        time.sleep(2)
-        j.value(0)
-    for i in range(len(leds)):
-        print("Game 2")
-        leds[i].value(1)
-        time.sleep(2)
-        leds[i].value(0)
-
-    if  x < 4:
-        print("Game 3")
-        leds[x].value(1)
-        time.sleep(2)
-        leds[x].value(0)       
-        time.sleep(2)
-        ++x
-
-    if y < 10:
-        print("Game 4")
-        for j in leds:
-            j.value(1)
-            time.sleep(2)
-            j.value(0)
-            time.sleep(2)
-            ++y
+# 5 times led1 blinks (toggle-> 1 to 0)  in 1 sec  
+    for i in range(10):
+        print("led1 blinks",i)
+        led1.toggle()
+        time.sleep(1)
+# 5 times led2 blinks with break in 10 ms
+    for j in range(10):
+        print("led2 blinks:",j)
+        led2.value(1)
+        time.sleep_ms(10)
+        led2.value(0)
+        time.sleep_ms(10) 
+        if j == 5:
+            break
+# 5 times led3 blinks with toggle fuction in 500 us
+    for k in range(5):
+        print("led3 blinks:",k)
+        led3.value(toggle)
+        toggle=toggle^1;
+        time.sleep_us(500)
+        led3.value(toggle)
+        time.sleep_ms(500)
+# 5 times led 4 blinks before all for cicles in 1 sec
+    if x<6:
+        print("led4 blinks:",x)
+        led4.toggle()
+        time.sleep(1)
+        ++x;
