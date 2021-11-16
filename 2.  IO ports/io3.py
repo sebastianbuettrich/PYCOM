6dbd9a72ce271a9aa8aa3b6b4fb758c7b0f97c0a
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-16 14:52:09 +0100

    Update io3.py

--- 2.  IO ports/io3.py
+++ 2.  IO ports/io3.py
@@ -1,10 +1,10 @@
 from machine import Pin
 import time
-led = Pin('P9', mode = Pin.OUT)
-button = Pin('P8', mode = Pin.IN)
+led = Pin('P9', mode = Pin.OUT) # pin 9 as output
+button = Pin('P8', mode = Pin.IN) # pin 8 as input
 
 while True:
-    if(button() == 0):
-        led.value(1)
+    if(button() == 0):  # state change condition
+        led.value(1)  #true, led on
     else:
-        led.value(0)
+        led.value(0) # false, led off
