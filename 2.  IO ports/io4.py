343f5d5fb675a292bc7ef9c6083f7724c3b71ce7
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-16 14:58:49 +0100

    Update io4.py

--- 2.  IO ports/io4.py
+++ 2.  IO ports/io4.py
@@ -1,15 +1,15 @@
 from machine import Pin
 import time
-led = Pin('P9', mode = Pin.OUT)
-button = Pin('P8', mode = Pin.IN)
-on=False
+led = Pin('P9', mode = Pin.OUT) # pin 9 as output
+button = Pin('P8', mode = Pin.IN) # pin 8 as output
+on=False  #bool variable
 while True:
-    if(button() == 0):
-        time.sleep_ms(200)
-        on=on^1
-        if on == True:
+    if(button() == 0):         # state change condition
+        time.sleep_ms(200)     # anti-bounce delay
+        on=on^1                # on changes state 
+        if on == True:         # condition
             print("led on")
-            led.value(1)
+            led.value(1)       # led on
         else:
             print("led off")
-            led.value(0)
+            led.value(0)       # led off
