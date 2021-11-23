48e476a19cbe6ad3ead4af118b7a45aacda16801
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-23 12:24:47 +0100

    Update main.py

--- 4. SERIAL CX/project1/main.py
+++ 4. SERIAL CX/project1/main.py
@@ -1,10 +1,17 @@
-from machine import ADC
+from machine import UART
 import time
-
-adc = ADC(0)
-adc_c = adc.channel(pin='P13')
-
+uart = UART(0, baudrate=115200)
+lst = []
 while True:
-    value = adc_c.value()
-    print("ADC value:" + str(value))
-    time.sleep(1)
+    if uart.any() > 0:   
+        data=uart.read()   # read all available characters
+        lst=list(data)     # convert data in list
+        for i in range (len(data)):
+            lst[i]= chr(lst[i])
+            lst[i]=lst[i].upper()
+            if lst[i] == 'A' or lst[i] == 'E' or lst[i] == 'I' or lst[i] == 'O' or lst[i] == 'U':
+                lst[i]='*' 
+        print(lst)
+
+
+    
