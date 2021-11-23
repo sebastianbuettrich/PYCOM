8a7036ae747da24f16cfc88670e965ced895c7c2
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-23 11:29:52 +0100

    Update cx2.py

--- 4. SERIAL CX/cx2.py
+++ 4. SERIAL CX/cx2.py
@@ -4,7 +4,6 @@ uart = UART(0, baudrate=115200)
 lst = []
 while True:
     if uart.any() > 0:   
-        print("yes")       # returns the number of characters waiting
         data=uart.read()   # read all available characters
         lst=list(data)     # convert data in list
         for a in range(len(lst)):  # for cicle in range 0 to tam list
