dc182f993e52ce7454317c3ec955da56667ee87d
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-29 18:55:41 +0100

    Update io3.py

--- 2.  IO ports/io3.py
+++ 2.  IO ports/io3.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, IT university of Copenhagen
+#
+# This software is licensed under the GNU GPL version 3 or any
+# later version, with permitted additional terms. For more information
+# see the Pycom Licence v1.0 document supplied with this file
+# Code: io3.py -> Simple Input configuration
+# IoT course
+#
+
 from machine import Pin
 import time
 led = Pin('P9', mode = Pin.OUT) # pin 9 as output
