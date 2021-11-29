62d081f30aa9a4655047ae7b70dc912a16f4dbde
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-29 18:58:32 +0100

    Update adc1.py

--- 3. ADC/adc1.py
+++ 3. ADC/adc1.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, IT university of Copenhagen
+#
+# This software is licensed under the GNU GPL version 3 or any
+# later version, with permitted additional terms. For more information
+# see the Pycom Licence v1.0 document supplied with this file
+# Code: adc1.py -> Simple analog digital test
+# IoT course
+#
+
 from machine import ADC
 import time
 
