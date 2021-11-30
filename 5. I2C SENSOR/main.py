e513964c22a4dbf629ad0e52018529347511a6e1
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-30 14:12:34 +0100

    Update main.py

--- 5. I2C SENSOR/main.py
+++ 5. I2C SENSOR/main.py
@@ -1,11 +1,18 @@
-from machine import Pin
 import time
-led = Pin('P9', mode=Pin.OUT)
+from machine import I2C, Pin
+from scd30 import SCD30
+
+
+i2c = I2C(2)
+scd30 = SCD30(i2c, 0x61)
 
 while True:
-    print("high")
-    led.value(1)
-    time.sleep(1)
-    print("low")
-    led.value(0)
-    time.sleep(1)
+    # Wait for sensor data to be ready to read (by default every 2 seconds)
+    while scd30.get_status_ready() != 1:
+        time.sleep_ms(200)
+    (co2,temp,hum)=scd30.read_measurement()
+        #-3
+    temp -=3
+    print("co2",co2)
+    print("temp",temp)
+    print("hum",hum)
