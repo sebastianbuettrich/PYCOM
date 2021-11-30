f8a99cf021a4934b9517ef32844be4a244b6165d
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2021-11-30 14:23:47 +0100

    Update i2c1.py

--- 5. I2C SENSOR/i2c1.py
+++ 5. I2C SENSOR/i2c1.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, IT university of Copenhagen
+#
+# This software is licensed under the GNU GPL version 3 or any
+# later version, with permitted additional terms. For more information
+# see the Pycom Licence v1.0 document supplied with this file
+# Code: cx1.py -> Read/Write chart/strings
+# IoT course
+#
+# Thanks Niels..!!
+#
+
+
 import time
 import pycom
 from machine import I2C
